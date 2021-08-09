3ee88e1fdaaf47315235eec644592fe5298bde12
technology-97 <rushikeshbhandare789@gmail.com>
2021-08-09 08:59:03 +0600

    Added:HotelReservation  program is Added

---
+++ HotelReservation.java
@@ -0,0 +1,15 @@
+package hotelreservation;
+
+public class HotelReservation {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("Welcome to the Hotel Reservation System");
+		
+	}
+	
+	
+
+}
