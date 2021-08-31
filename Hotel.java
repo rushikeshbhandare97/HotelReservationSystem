2a5ec3e1a1b6f9339bdcf6204a9fea6170690376
rushikeshbhandare <86002633+rushikeshbhandare97@users.noreply.github.com>
2021-08-31 22:35:03 +0530

    Create Hotel.java

---
+++ Hotel.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+/**
+
+ * creating hotel class and main class
+ */
+public class Hotel {
+    public static void main(String[] args) {
+        System.out.println("Welcome to hotel reservation system");
+        System.out.println("Welcome to hotel reservation system");//welcome message
+/**
+ * adding rate for hotel each a every hotel name
+ */
+        HotelReservationSystem lakewood = new HotelReservationSystem("lakewood");
+        lakewood.setWeekDayRegularRate(110);
+        lakewood.setWeekDayRewardRate(80);
+
+        HotelReservationSystem Bridgewood = new HotelReservationSystem("Bridgewood");
+        Bridgewood.setWeekDayRegularRate(160);
+        Bridgewood.setWeekDayRewardRate(110);
+
+        HotelReservationSystem Ridgewood = new HotelReservationSystem("Ridgewood");
+        Ridgewood.setWeekDayRegularRate(220);
+        Ridgewood.setWeekDayRewardRate(100);
+    }
+}
